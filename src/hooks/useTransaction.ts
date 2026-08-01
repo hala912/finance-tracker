704721b9fb6a0ee14ac0e64b7c3ae5b5a00aa47b
hala912 <hala19717131@gmail.com>
2026-08-01 21:42:05 +0300

    Phase4 : server-side pageniation

--- src/hooks/useTransaction.ts
+++ src/hooks/useTransaction.ts
@@ -1,23 +1,33 @@
 import { useQuery } from "@tanstack/react-query"
 import { useAuth } from "../context/AuthContext"
 import { supabase } from "../Api/supabase"
+import { useState } from "react"
 
 export function useTransaction() {
 
+    const pageSize = 10 
+    const [page,setPage] = useState(0)
+     const from = page * pageSize
+     const to = from + (pageSize-1)
     const { user } = useAuth() 
-    return useQuery(
+
+    const query =  useQuery(
         {
-            queryKey : ['transactions', user?.id],
+            queryKey : ['transactions', user?.id, page],
             queryFn : async ()=>{
-                const {data , error}= await supabase
+                const {data , error, count}= await supabase
                 .from('transactions')
-                .select('*')
+                .select('*', {count : 'exact'})
                 .eq('user_id', user!.id)
+                .range(from , to )
+                
                 
                 if(error) throw error
-                return data
+                return {data,count}
             },
             enabled: !!user
         }
     ) 
+     return { ...query, page, setPage }  
 }
+
